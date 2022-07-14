896b65b14b91954c41ce192750d57eb7a45a439c
Hugomelo <salles.hugo@gmail.com>
2022-07-14 11:33:16 -0400

    learning arrays and objects with labs

---
+++ array.js
@@ -0,0 +1,39 @@
+console.log("Hello World");
+/** Create an array with a list of fruits starting with 3 fruit
+
+with that array now do the following
+- Add a new fruit to the end of the array
+- Display the array
+- Add a new fruit to the beginning of the array
+- Display the array
+- Remove the last fruit.
+- Display the array */
+
+// adding / removing
+// let fruits = ["kiwi", "mango", "apple"];
+// fruits.push("avocado");
+// console.log(fruits);
+
+// fruits.unshift("carambola");
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+
+// fruits.sort();
+// console.log(fruits);
+
+// //sorting / indexing
+// console.log(fruits.indexOf("hugo"));
+
+
+const chickenEgg = ['🐓', '🥚'].sort()
+console.log(`the ${chickenEgg[0]} came first`);
+
+
+// console.log(...fruits);
+// //this is regular display ....
+// console.log(`this is regular display ${fruits}`)
+// //this is spread .....
+// console.log('this is a spead', fruits);
+// console.log('this is a spead', ...fruits);
